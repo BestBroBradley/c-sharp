84dd7f9146f94de941de4355210dcf73c2107a8e
Allison Bradley <allison.bradley@outlook.com>
2021-02-06 11:03:23 -0700

    Added some light error handling

--- app.cs
+++ app.cs
@@ -1,11 +1,20 @@
 using System;
 using System.IO;
 
-class Writer
+class CommitWriter
 {
     static void Main(string[] args)
     {
         string path = Directory.GetCurrentDirectory() + "/data/commits.txt";
+        try
+        {
         System.IO.File.WriteAllText(path, "testing");
+        Console.WriteLine("Successfully wrote commit history to commits.txt");
+        }
+        catch (Exception ex)
+        {
+        // Not sure if this error handling is correct--how to test??
+        Console.WriteLine(ex);
+        }
     }
 }
