8b41453e2384200b8ab074d764d2f9de68fa8d5c
Allison Bradley <allison.bradley@outlook.com>
2021-02-06 15:42:46 -0700

    Working on API connection

--- CommitWriter/API/API.cs
+++ CommitWriter/API/API.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace CommitWriter.API
 {
+    //  This class is the starter for contacting an API
+    public static class APIInit
+    {
+        public static HttpClient APIClient { get; set; }
+
+        public static void InitializeClient()
+        {
+            APIClient = new HttpClient();
+            APIClient.DefaultRequestHeaders.Accept.Clear();
+            APIClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            Console.WriteLine("Connection initialized");
+        }
+    }
+
     public class APICalls
     {
-        public static void GetCommits()
+        public async Task GetCommits()
         {
-            Console.WriteLine("This is a test!");
+            string url = "https://api.bitbucket.org/2.0/repositories/calanceus/interviews17/commits";
+
+            using (HttpResponseMessage response = await APIInit.APIClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.Write(response);
+                }
+            };
         }
     }
 }
--- CommitWriter/Program.cs
+++ CommitWriter/Program.cs
@@ -1,14 +1,32 @@
 ﻿using System;
+using System.IO;
 using CommitWriter.API;
 
 namespace CommitWriter
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            APICalls.GetCommits();
+            void WriteData()
+            {
+                string path = Directory.GetCurrentDirectory() + "/data/commits.txt";
+                try
+                {
+                    System.IO.File.WriteAllText(path, "testing");
+                    Console.WriteLine("Successfully wrote commit history to commits.txt");
+                }
+                catch (Exception ex)
+                {
+                    // Not sure if this error handling is correct--how to test??
+                    Console.WriteLine(ex);
+                }
+            }
+
+            APIInit.InitializeClient();
+            WriteData();
+            //APICalls.GetCommits();
         }
+
     }
 }
