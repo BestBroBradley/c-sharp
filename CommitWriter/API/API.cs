af1b91a9761721eaa3d93ab8ffed3b8f4fea0e17
Allison Bradley <allison.bradley@outlook.com>
2021-02-06 14:36:29 -0700

    Switched over to VS

---
+++ CommitWriter/API/API.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommitWriter.API
+{
+    public class APICalls
+    {
+        public static void GetCommits()
+        {
+            Console.WriteLine("This is a test!");
+        }
+    }
+}
---
+++ CommitWriter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using CommitWriter.API;
+
+namespace CommitWriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            APICalls.GetCommits();
+        }
+    }
+}
